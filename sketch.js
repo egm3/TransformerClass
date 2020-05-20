977009e0b1d96ef00f3698476980e5646f2dd147
egm3 <egm3@cin.ufpe.br>
2020-05-20 13:10:32 -0300

    20, may, 13:10

---
+++ sketch.js
@@ -0,0 +1,20 @@
+let angle = 0.0;
+let x = 500;
+function setup() {
+  createCanvas(1000, 1000);
+  angleMode(DEGREES);
+  ellipseMode(CORNER);
+}
+
+function draw() {
+  background(0);
+  
+  push();
+  translate(width/2, width/2);
+  for(var i = 0; i < x; i+=2) {
+    scale(0.95);
+    rotate(angle);
+    ellipse(i*20+20, 0, 20+i, 20+i);
+  }
+  angle+= 0.1;
+}
